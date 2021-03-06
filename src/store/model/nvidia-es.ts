be595b202b46e9f3c682d2ab09e5da4216e4d1b3
Alex Vazquez <vazquinhos@gmail.com>
2021-03-06 14:44:41 +0100

    Add new ldlc Nvidia links

--- src/store/model/nvidia-es.ts
+++ src/store/model/nvidia-es.ts
@@ -4,8 +4,8 @@ export const NvidiaES: Store = {
   currency: '€',
   labels: {
     inStock: {
-      container: '.buy',
-      text: ['Añadir al carrito', 'COMPRAR AHORA'],
+      container: '.add-to-cart-bloc',
+      text: ['Añadir a la cesta', 'COMPRAR YA'],
     },
     outOfStock: {
       container: '.buy',
@@ -30,9 +30,23 @@ export const NvidiaES: Store = {
     {
       brand: 'nvidia',
       model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.ldlc.com/es-es/ficha/PB66603060.html',
+    },
+    {
+      brand: 'nvidia',
+      model: 'founders edition',
       series: '3070',
       url:
         'https://www.nvidia.com/es-es/shop/geforce/gpu/?page=1&limit=9&locale=es-es&category=GPU&gpu=RTX%203070&manufacturer=NVIDIA',
+    },
+    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3070',
+      url:
+        'https://www.ldlc.com/es-es/ficha/PB66603070.html',
     },
     {
       brand: 'nvidia',
@@ -44,9 +58,23 @@ export const NvidiaES: Store = {
     {
       brand: 'nvidia',
       model: 'founders edition',
+      series: '3080',
+      url:
+        'https://www.ldlc.com/es-es/ficha/PB99993080.html',
+    },
+    {
+      brand: 'nvidia',
+      model: 'founders edition',
       series: '3090',
       url:
         'https://www.nvidia.com/es-es/shop/geforce/gpu/?page=1&limit=9&locale=es-es&category=GPU&gpu=RTX%203090&manufacturer=NVIDIA',
+    },
+    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3090',
+      url:
+        'https://www.ldlc.com/es-es/ficha/PB66603090.html',
     },
   ],
   name: 'nvidia-es',
